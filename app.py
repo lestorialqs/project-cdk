da0d64dae843aa9b325b2531a8d86a1232e49f48
Henry Quispe Sutta <Henryqs>
2025-04-17 11:46:56 -0500

    Stack vacio simple

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import aws_cdk as cdk
+from constructs import Construct
 
-from proyecto_cdk.proyecto_cdk_stack import VmStack
 
+
+class TestStack(cdk.Stack):
+    def __init__(self, scope: Construct, id: str, *, execute_role_arn: str = None, **kwargs):
+        super().__init__(scope, id, execute_role_arn=execute_role_arn, **kwargs)
+
+        
 app = cdk.App()
-VmStack(app, "VmStack",
+TestStack(app, "TestStack",
     env=cdk.Environment(account="708642711016", region="us-east-1"),
-    execute_role_arn="arn:aws:iam::708642711016:role/LabRole"
+    execute_role_arn="arn:aws:iam::708642711016:role/LabRole" 
 )
 
 app.synth()
