aff89510a98b99aac72550f2d8c68f6261cef5db
Henry Quispe Sutta <Henryqs>
2025-04-17 11:54:17 -0500

    Ejemplo stack vacio

--- app.py
+++ app.py
@@ -4,14 +4,13 @@ from constructs import Construct
 
 
 class TestStack(cdk.Stack):
-    def __init__(self, scope: Construct, id: str, *, execute_role_arn: str = None, **kwargs):
-        super().__init__(scope, id, execute_role_arn=execute_role_arn, **kwargs)
+    def __init__(self, scope: Construct, id: str, str = None, **kwargs):
+        super().__init__(scope, id, **kwargs)
 
         
 app = cdk.App()
 TestStack(app, "TestStack",
-    env=cdk.Environment(account="708642711016", region="us-east-1"),
-    execute_role_arn="arn:aws:iam::708642711016:role/LabRole" 
+    env=cdk.Environment(account="708642711016", region="us-east-1")
 )
 
 app.synth()
