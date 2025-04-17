8477470e98d8c64925062c3d7fdbafc5e7e42b45
Henry Quispe Sutta <Henryqs>
2025-04-17 17:18:05 -0500

    Sintetizador para el rol de ejecucion en el constructor

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 import aws_cdk as cdk
 
+from aws_cdk import DefaultStackSynthesizer
+
 from proyecto_cdk.proyecto_cdk_stack import VmStack
 
 
 app = cdk.App()
 
-custom_execution_role = 'arn:aws:iam::708642711016:role/LabRole'
+custom_execution_role = "arn:aws:iam::708642711016:role/LabRole"
+
+synth = DefaultStackSynthesizer(
+    cloud_formation_execution_role=custom_execution_role
+)
 
 
 VmStack(app, "VmStack",
     env=cdk.Environment(account="708642711016", region="us-east-1"),
-    role=custom_execution_role
-)
+    synthesizer=synth
+)   
 
 app.synth()
