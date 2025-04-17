7dd34416d4c30cb20ffbb6bc38967ac4cefd3825
Henry Quispe Sutta <Henryqs>
2025-04-17 12:39:51 -0500

    Problema solucionado en consola

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import aws_cdk as cdk
-from constructs import Construct
+
+from proyecto_cdk.proyecto_cdk_stack import VmStack
 
 
-
-class TestStack(cdk.Stack):
-    def __init__(self, scope: Construct, id: str, str = None, **kwargs):
-        super().__init__(scope, id, **kwargs)
-
-        
 app = cdk.App()
-TestStack(app, "TestStack",
+VmStack(app, "VmStack",
     env=cdk.Environment(account="708642711016", region="us-east-1")
 )
 
--- proyecto_cdk/proyecto_cdk_stack.py
+++ proyecto_cdk/proyecto_cdk_stack.py
@@ -6,8 +6,8 @@ from constructs import Construct
 import aws_cdk.aws_iam as iam
 
 class VmStack(Stack):
-    def __init__(self, scope: Construct, id: str, *, execute_role_arn: str = None, **kwargs):
-        super().__init__(scope, id, execute_role_arn=execute_role_arn, **kwargs)
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
 
         # Traer VPC predeterminada
         vpc = ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True)
