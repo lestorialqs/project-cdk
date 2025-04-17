34a64aa818ee9d2e75089fa2f053844c237c93a8
Henry Quispe Sutta <Henryqs>
2025-04-17 15:51:22 -0500

    Role de ejecucion en el constructor de la stack

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from proyecto_cdk.proyecto_cdk_stack import VmStack
 
 
 app = cdk.App()
+
+custom_execution_role = 'arn:aws:iam::708642711016:role/LabRole'
+
+
 VmStack(app, "VmStack",
-    env=cdk.Environment(account="708642711016", region="us-east-1")
+    env=cdk.Environment(account="708642711016", region="us-east-1"),
+    role=custom_execution_role
 )
 
 app.synth()
