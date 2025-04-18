3996f26950261604b7b586cfa46d3406c734b4cb
Henry Quispe Sutta <Henryqs>
2025-04-18 18:01:10 -0500

    Ultima actualizacion eliminacion de container ECR

--- proyecto_cdk/proyecto_cdk_stack.py
+++ proyecto_cdk/proyecto_cdk_stack.py
@@ -15,19 +15,20 @@ class VmStack(Stack):
         # Obtener una subnet pública de esa VPC
         subnet_id = vpc.public_subnets[0].subnet_id
 
-        # Grupo de seguridad
+        # Crear grupo de seguridad
         sg = ec2.SecurityGroup(self, "SG",
             vpc=vpc,
             description="Permitir SSH y HTTP",
             allow_all_outbound=True
         )
+        #Permitir el acceso ssh y http
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(22), "SSH")
         sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "HTTP")
 
-        # Instancia EC2 usando CfnInstance
+        # Crear Instancia EC2 usando CfnInstance
         ec2.CfnInstance(self, "EC2Instance-aws-cdk",
             image_id="ami-0f2d4b21b93e88520",  # Cloud9 Ubuntu22 mas reciente
-            instance_type="t2.micro",
+            instance_type="t2.micro", #Tipo de la instancia
             key_name="vockey",
             subnet_id=subnet_id,
             security_group_ids=[sg.security_group_id],
@@ -35,7 +36,7 @@ class VmStack(Stack):
                 "deviceName": "/dev/xvda",
                 "ebs": {
                     "volumeSize": 20,
-                    "volumeType": "gp2"
+                    "volumeType": "gp2" 
                 }
             }]
         )
